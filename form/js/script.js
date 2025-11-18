7a374a53f13be9e5b81fdbd7ebbff83c44e90ad7
Yaramir <777vsem97@mail.ru>
2025-11-18 18:41:04 +0300

    Добавил стиль формы и js валидацию

---
+++ form/js/script.js
@@ -0,0 +1,29 @@
+const form = document.querySelector('.form-input');
+
+form.addEventListener('submit', (event) => {
+event.preventDefault();
+
+const nameIn = document.querySelector('#in-name');
+const mail = document.querySelector('#in-mail');
+const mess = document.querySelector('#message');
+
+const oldMsg = form.querySelector('.form-msg')
+if(oldMsg) oldMsg.remove()
+
+if(!nameIn.value || !mail.value || !mess.value) {
+    const div = document.createElement('div');
+    div.classList.add('form-msg', 'form-msg-error')
+    div.textContent = 'Заполните поля для отправки';
+    form.appendChild(div)
+    return;
+} else {
+    const div2 = document.createElement('div');
+    div2.classList.add('form-msg', 'form-msg-success')
+    div2.textContent = 'Сообщение отправлено';
+    form.appendChild(div2);
+    nameIn.value = '';
+    mail.value = '';
+    mess.value = '';
+}
+})
+
